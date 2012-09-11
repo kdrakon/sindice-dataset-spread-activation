a329ef0134c9fb92694cb6b9596dec745c4ef076
kdrakon <kdrakon@gmail.com>
2012-09-11 18:26:01 +0100

    backend can now retrieve SPARQL sindice results and model into JSON

--- src/backend.js
+++ src/backend.js
@@ -10,12 +10,58 @@
  */
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
-var express = require("express");
+var express = require('express');
+var $ = require('jquery');
+var _ = require('underscore');
  
 /*
  * Global Variables and Constants
  */	
-// none yet
+var predef_Sindice_query = "PREFIX analytics: <http://vocab.sindice.net/analytics#>  PREFIX any23: <http://vocab.sindice.net/>   SELECT DISTINCT ?class_label, ?class_card, ?property, ?property_card FROM <http://sindice.com/analytics> WHERE {       ?class any23:domain_uri <http://sindice.com/dataspace/default/domain/bbc.co.uk>	 .           ?class analytics:cardinality ?class_card.           ?class analytics:label ?bnode .           ?bnode analytics:label ?class_label .      ?bnode analytics:rank1 '1'^^xsd:integer.       ?edge analytics:publishedIn <http://sindice.com/dataspace/default/domain/bbc.co.uk>.      ?edge analytics:source ?class.       ?edge analytics:label ?property.      ?edge analytics:cardinality ?property_card.         FILTER(?class_card > 100)      FILTER(?property_card > 100)  }";
+
+/*
+ * Function Definitions
+ */
+function constructQuery(variables){
+    // TODO
+}
+
+/* Turn the JSON returned result from Sindice into a graph model */
+function prepareGraphModel(data){
+    
+    // initialise the graph model
+    var model = { domain : "dummy.org", childClasses : {}, childClassProperties : {} };
+    
+    // get the SPARQL binding results
+    var bindings = data.results.bindings;
+    
+    // for each result, incrementally construct the graph model
+    _.each(bindings, function(r){
+        
+        // create instances of the child classes and their cardinalities
+        var classURI = r['class_label'].value;
+        var classCard = r['class_card'].value;        
+        
+        // add the child class
+        model.childClasses[classURI] = classCard;
+        
+        // for each child class, append their associated properties and cardinalities
+        var propertyURI = r['property'].value;
+        var propertyCard = r['property_card'].value;
+        
+        // initialise the object if it hasn't been already
+        if ( !_.isObject(model.childClassProperties[classURI]) ){
+            model.childClassProperties[classURI] = {};
+        }
+        // add the child properties
+        model.childClassProperties[classURI][propertyURI] = propertyCard;        
+        
+    });
+    
+    return model;
+    
+}
+
 
 /*
  * Node.js Server Program
@@ -25,21 +71,33 @@ var express = require("express");
 console.log("starting server...");
 var server = express();
 
-// parse HTTP requests into JSON
+// tell express to parse HTTP requests into JSON
 server.use(express.bodyParser());
 
 // serve the webpage folder as a normal HTTP server
 server.use(express.static('../webpage'));
 
-// handle posts of ASP programs to the server
+// handle GET requests to the server
 server.get('/activate', function(req, res){
 
     // GET request variables are available in req.query
     
-    var sindiceSPARQLquery = req.query['squery'];
+    // get the URI of the dataset(s) to query
+    var sindice_DatasetURI = req.query['dataset_uri'];
     
-    res.send(200);
-  
+    // communicate with Sindice SPARQL ep to retrieve analytics
+    model = $.ajax({
+       url: 'http://sparql.sindice.com/sparql?query=' + escape(predef_Sindice_query),
+       dataType: 'json',
+       success: function(data, textStatus, jqXHR){
+           // turn the returned data into the graph model
+           res.send(prepareGraphModel(data));
+       },
+       error: function(jqXHR, textStatus, errorThrown){
+           console.log(textStatus + " " + errorThrown);
+       }
+    });
+      
 });
 
 /*
